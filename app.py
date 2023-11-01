c2125afc360fac5a3f1564b3ff9d928858d709aa
khrisn20 <khristian.narvaez@yahoo.com>
2023-11-01 13:14:02 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st  
+
+#Configurations for the page
+st.set_page_config(page_title="My Portfolio")
+
+#Header Section
+st.subheader("Hi, I am Christian")
+st.title("A Software Developer based in Tampa, FL")
+st.write("I have 4 years of IT experience with the military in which I have been tasked with roles in different areas of competency.")
+st.write("[Git Hub >](https://github.com/khrisn20)")
+
